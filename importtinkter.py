99215d153b7efe114099c32230a1538c849d5d59
SIDDHANT JAIN <32744277+jain-sidhnt@users.noreply.github.com>
2019-01-07 19:38:38 +0530

    Make Notepad using Tkinter
    Simple notepad in Python using Tkinter. This notepad GUI will consist of various menus like file and edit, using which all functionalities like saving the file, opening a file, editing, cut and paste can be done.
     Python 3 and Tkinter should already be installed in your system.

---
+++ importtinkter.py
@@ -0,0 +1,11 @@
+
+import tkinter  
+import os  
+from tkinter import *
+  
+# To get the space above for message 
+from tkinter.messagebox import *
+  
+# To get the dialog box to open when required 
+from tkinter.filedialog import *
+
